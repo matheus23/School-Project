2f18f44337bc467bfc35ea15e09bf2f3b5774bad
MrFol <lucaleitzbach@yahoo.de>
2014-01-28 08:46:50 +0100

    Benutzerkontenverwaltung editiert

--- fileshare/php/frontend/benutzerkonto.php
+++ fileshare/php/frontend/benutzerkonto.php
@@ -34,6 +34,35 @@ $menu->add(new Menupunkt("konto","Benutzerkonto","benutzerkonto.php",true));
 	</div>
 	<div id="panel">
 		<h1>Benutzerkontenverwaltung</h1>
+		<form method="POST" action="">
+		<table>
+			<tr><td>Nutername ändern:</td></tr>
+			<tr><td>Email:</td><td><input type="text" name="mail" id="mail"></td></tr>
+			<tr><td>Aktuelles Passwort:</td><td><input type="password" name="pwa" id="pwa"></td></tr>
+			<tr><td>Neuer Nutzername:</td><td><input type="password" name="pwn" id="pwn"><td></tr>
+			<tr><td colspan="2"><input type="submit" value="Nutzername ändern" name="namea"></td></tr>
+		</table>
+		</form>
+		<br>
+		<form method="POST" action="">
+		<table>
+			<tr><td>Email ändern:</td></tr>
+		    <tr><td>Email:</td><td><input type="text" name="mail" id="mail"></td></tr>
+		    <tr><td>Neue Email:</td><td><input type="password" name="pwa" id="pwa"></td></tr>
+		    <tr><td colspan="2"><input type="submit" value="Email ändern" name="emaila"></td></tr>
+		</table>
+		</form>
+		<br>
+		<form method="POST" action="">
+		<table>
+			<tr><td>Passwort ändern:</td></tr>
+			<tr><td>Email:</td><td><input type="text" name="mail" id="mail"></td></tr>
+			<tr><td>Aktuelles Passowort:</td><td><input type="password" name="pwa" id="pwa"></tr></td>
+			<tr><td>Neues Passwort:</td><td><input type="password" name="pwn" id="pwn"></td></tr>
+			<tr><td>Passwort wiederholen:</td><td><input type="password" name="pwn2" id="pwn2"></td></tr> 
+			<tr><td colspan="2"><input type="submit" value="Passwort ändern" name="passworta"></td></tr>
+		</form>
+		</table>		
 	</div>
 </body>
 </html>
