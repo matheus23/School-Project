b6bb9ab41d0055a35798899f1728ede6988ea4df
Yan97he <yherrling@t-online.de>
2014-02-05 10:56:17 +0100

    jetzt richtige Email-ändern-Seite, die noch eingebunden werden muss (Luca)

--- fileshare/php/websiteFunktionen/emailaenderung.php
+++ fileshare/php/websiteFunktionen/emailaenderung.php
@@ -8,20 +8,19 @@
 		$neueemail = $db->real_escape_string(strtolower($neueEmailUnencoded));
 		
 		if (userExestiertBereits($db, $email)) {
-				$passwortTest = benutzerPwTest($db, $email, $pw);
-				if ($passwortTest == PASSWORD_PASS)  {
-					$db->query("UPDATE `Benutzer` SET Email='$neueemail' WHERE Email='$email'")->fold(
-						function($ergebnis) use (&$nrt) {
-							$nrt->okay("Email erfolgreich geändert");
-							//Bestätigungs Email, dass das Pw geändert wurde
-						}, function($fehlerNachricht) use (&$nrt) {
-							$nrt->fehler("Es gab einen Fehler beim Datenbankzugriff: $fehlerNachricht");
-						}
-					);
-				} else {
-					$nrt->fehler("Email-Passwort Kombination passt nicht.");
-				}
-			}
+			$passwortTest = benutzerPwTest($db, $email, $pw);
+			if ($passwortTest == PASSWORD_PASS)  {
+				$db->query("UPDATE `Benutzer` SET Email='$neueemail' WHERE Email='$email'")->fold(
+					function($ergebnis) use (&$nrt) {
+						$nrt->okay("Email erfolgreich geändert");
+						//Bestätigungs Email, dass das Pw geändert wurde
+					}, function($fehlerNachricht) use (&$nrt) {
+						$nrt->fehler("Es gab einen Fehler beim Datenbankzugriff: $fehlerNachricht");
+					}
+				);
+			} else {
+				$nrt->fehler("Email-Passwort Kombination passt nicht.");
+			}	
 		}
 	}
 ?>
