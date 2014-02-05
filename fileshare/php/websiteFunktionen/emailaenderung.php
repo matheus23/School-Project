d546796d757eba5c057c779ca3a91c3bcc6c230c
Yan97he <yherrling@t-online.de>
2014-02-05 10:51:07 +0100

    Email-ändern-Seite, die noch eingebunden werden muss (Luca)

---
+++ fileshare/php/websiteFunktionen/emailaenderung.php
@@ -0,0 +1,27 @@
+<?php
+	include "../utilities.php";
+
+	function verarbeiteEmailaenderung($nrt, $emailUnencoded, $pw, $neueEmailUnencoded) {
+		$db = oeffneBenutzerDB($nrt);
+	
+		$email = $db->real_escape_string(strtolower($emailUnencoded));
+		$neueemail = $db->real_escape_string(strtolower($neueEmailUnencoded));
+		
+		if (userExestiertBereits($db, $email)) {
+				$passwortTest = benutzerPwTest($db, $email, $pw);
+				if ($passwortTest == PASSWORD_PASS)  {
+					$db->query("UPDATE `Benutzer` SET Email='$neueemail' WHERE Email='$email'")->fold(
+						function($ergebnis) use (&$nrt) {
+							$nrt->okay("Email erfolgreich geändert");
+							//Bestätigungs Email, dass das Pw geändert wurde
+						}, function($fehlerNachricht) use (&$nrt) {
+							$nrt->fehler("Es gab einen Fehler beim Datenbankzugriff: $fehlerNachricht");
+						}
+					);
+				} else {
+					$nrt->fehler("Email-Passwort Kombination passt nicht.");
+				}
+			}
+		}
+	}
+?>
